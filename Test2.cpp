ee829ee437cdbeb19e36cf101cc71ca66accc48a
EYATUPE <eyatuphilipesamu@gmail.com>
2024-02-07 12:10:11 +0300

    exercise2

---
+++ Test2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int x = 564; //declare x variable
+	int y = 64; //declare y variable
+	// output the sum of two digits on the terminal
+	cout<<"The sum of x and y is"<< x + y<<endl;
+return 0;
+}
