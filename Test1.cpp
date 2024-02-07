739a1b632a812fee6e1fe137b85b226b1bdb4809
EYATUPE <eyatuphilipesamu@gmail.com>
2024-02-07 11:21:14 +0300

    exercise

---
+++ Test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	//printing data types and variable declarations
+	int = x
+	
+		float = y
+		double = z
+		char = i
+	string = p
+	//cout a sentence on the terminal
+	cout<<"What's Your genda?"<<endl;
+return 0;
+}
