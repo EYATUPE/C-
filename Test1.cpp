3c589f4ff845d5c83b5cf350ec234e6960896f01
EYATUPE <eyatuphilipesamu@gmail.com>
2024-02-07 11:24:11 +0300

    exercise

--- Test1.cpp
+++ Test1.cpp
@@ -2,14 +2,13 @@
 #include <string>
 using namespace std;
 int main(){
-	//printing data types and variable declarations
-	int = x
-	
-		float = y
-		double = z
-		char = i
-	string = p
-	//cout a sentence on the terminal
+	//different data types and variable declarations
+	int x;
+	char y;
+	float z;
+	double r;
+sting;
+	//cout to print out information on the terminal
 	cout<<"What's Your genda?"<<endl;
 return 0;
 }
