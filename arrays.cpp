d622e8a59f3d77d8278cbe1014dc597b855998d8
EYATUPE <eyatuphilipesamu@gmail.com>
2024-02-07 17:59:16 +0300

    arrays

---
+++ arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::count;
+using std::cin;
+using std::endl;
+
+int main(){
+	//Write the C++ Code Here
+	//DECLARING THE VARIABLES TO BE USED
+	string name;// name of the respondent
+	
+	int age;
+	float height;
+	float weight;
+
+}
