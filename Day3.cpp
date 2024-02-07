06ec6f0c131317b1621778bc1a6ab2f7b47acd4b
EYATUPE <eyatuphilipesamu@gmail.com>
2024-02-07 11:01:49 +0300

    Day3

---
+++ Day3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string x ;
+	string y ;
+	cout<<"What's your First Name "<<endl;
+	cin>>x;
+	cout<<"What's your Second Name "<<endl;
+	cin>>y;
+	cout<<"Hello "<< x +" "+ y <<endl;
+	
+	return 0;
+}
